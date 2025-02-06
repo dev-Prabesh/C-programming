8b197e74763888e3e0c92359f32db6d6d717eb80
Prabesh <prabesh5511@gmail.com>
2025-02-07 02:03:36 +0545

    add operator in c

---
+++ 07operatorinc/operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 1;
+    int b = 2;
+    int c = 5;
+
+    int calculate = (a + b) * c ;
+    
+    printf("The output is: %d",calculate);
+
+    return 0;
+
+}
