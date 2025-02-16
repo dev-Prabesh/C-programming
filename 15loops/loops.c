79fd5a1f775f44f842d73ca71590cbab8868a511
Prabesh <prabesh5511@gmail.com>
2025-02-17 04:13:43 +0545

    add loops in c

---
+++ 15loops/loops.c
@@ -0,0 +1,65 @@
+//  FOR LOOP
+
+// #include <stdio.h>
+
+// int main() {
+    
+//     int i;
+
+//     for(i=0; i<=10; i++) {
+//       printf("Prabesh Dahal \n");
+//       printf("%d \n", i);
+//     }
+
+//     return 0;
+// }
+
+// end line of for loop.
+
+//......................................................
+
+// WHILE LOOP
+
+// #include <stdio.h>
+
+// int main() {
+
+//     int i = 1;
+//     while(i <= 50){
+//         printf("%d learning while loop in c \n", i);
+//         i++;
+//     }
+     
+//     return 0;
+// }
+
+// end line of while loop.
+
+//..................................................
+
+// DO WHILE LOOP IN C  
+
+#include <stdio.h>
+
+int main() {
+
+    int i = 1;
+
+    do{
+        printf("hello %d \n", i);
+        i++;
+    } while (i <= 100);
+
+    //.....................................
+
+    // int i = 50;
+
+    // do{
+    //     printf("hello %d \n", i);
+    //     i--;
+    // } while (i >= 1);
+
+    return 0;
+}
+
+// end line of do while loop.
